d302e8bd93a938a9c070d16120df9db51677b13b
Eddy <3ddy.rakoto@gmail.com>
2021-07-07 09:43:26 +0300

    creation entity voiture

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VoitureRepository::class)
+ */
+class Voiture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $immatriculation;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $couleur;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kilometrage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): self
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): self
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getKilometrage(): ?int
+    {
+        return $this->kilometrage;
+    }
+
+    public function setKilometrage(int $kilometrage): self
+    {
+        $this->kilometrage = $kilometrage;
+
+        return $this;
+    }
+}
