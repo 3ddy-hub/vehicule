2824db5d5e26b453686340bbce629669dce46083
Eddy <3ddy.rakoto@gmail.com>
2021-07-07 09:48:56 +0300

    creation entity modele

---
+++ src/Entity/Modele.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModeleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ModeleRepository::class)
+ */
+class Modele
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $modele;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $marque;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $puissance;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $carburant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getPuissance(): ?string
+    {
+        return $this->puissance;
+    }
+
+    public function setPuissance(string $puissance): self
+    {
+        $this->puissance = $puissance;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): self
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+}
